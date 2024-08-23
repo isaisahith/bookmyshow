e06c088a4b6d6130d2b661e1729acd850924776e
isaisahith <saisahithinakota@gmail.com>
2024-08-23 20:31:19 +0530

    23-08-2024 20:31

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -7,22 +7,22 @@ import { Description } from './Description/Description';
 export const Movie = ({movie}) => {
     const {name, description} = movie;
   return (
-    <div className='movie'>
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={movie.posterUrl} />
+    <div >
+        <Card style={{ width: '18rem' }} className='movie'>
+      <Card.Img className='cardImage' variant="top" src={movie.posterUrl} />
       <Card.Body>
         <Card.Title>{movie.name}</Card.Title>
         <Description description={description}/>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>Language: {movie.language}</ListGroup.Item>
+        
         <ListGroup.Item>Directed by {movie.director}</ListGroup.Item>
         <ListGroup.Item>released on {movie.releaseDate}</ListGroup.Item>
       </ListGroup>
-      <Card.Body>
+      {/* <Card.Body>
         <Card.Link href="#">Card Link</Card.Link>
         <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
+      </Card.Body> */}
     </Card>
     </div>
   )
